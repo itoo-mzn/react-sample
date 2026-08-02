71db32faba824325e0236ba5ce8b76d9635b51c2
Takuya Ito <ito0804takuya@gmail.com>
2026-08-02 16:52:39 +0900

    mouseEnter

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,14 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import EventMouse from "./EventMouse.jsx";
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
+    <EventMouse
+      alt="ロゴ画像"
+      defaultSrc="https://www.web-deli.com/image/linkbanner_l.gif"
+      afterSrc="https://www.web-deli.com/image/home_chara.gif"
+    />
   </StrictMode>,
-)
+);
