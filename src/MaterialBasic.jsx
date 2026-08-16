af5c8855fe8555536e4e5e0f7c078579a4775db6
Takuya Ito <ito0804takuya@gmail.com>
2026-08-16 18:24:36 +0900

    MUIによるボタンの見た目確認

---
+++ src/MaterialBasic.jsx
@@ -0,0 +1,13 @@
+import { Button } from "@mui/material";
+
+export default function MaterialBasic() {
+  return (
+    <>
+      <Button variant="text">text</Button>
+      <Button variant="contained" color="secondary">
+        contained
+      </Button>
+      <Button variant="outlined">outlined</Button>
+    </>
+  );
+}
--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,9 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import ErrorRoot from "./ErrorRoot.jsx";
+import MaterialBasic from "./MaterialBasic.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <ErrorRoot />
+    <MaterialBasic />
   </StrictMode>,
 );
