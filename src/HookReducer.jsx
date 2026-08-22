4e90d0629293ab0a686f4627ca2b6002d912b11d
Takuya Ito <ito0804takuya@gmail.com>
2026-08-22 20:31:41 +0900

    複数のアクションに対応するReducer

--- src/HookReducer.jsx
+++ src/HookReducer.jsx
@@ -7,7 +7,9 @@ export default function HookReducer({ init }) {
     (state, action) => {
       switch (action.type) {
         case "update":
-          return { count: state.count + 1 };
+          return { count: state.count + action.step };
+        case "reset":
+          return { count: action.init };
         default:
           return state;
       }
@@ -16,14 +18,22 @@ export default function HookReducer({ init }) {
     { count: init },
   );
 
-  const handleClick = () => {
+  const handleUp = () => {
     // Reducer経由でStateを更新
-    dispatch({ type: "update" });
+    dispatch({ type: "update", step: 1 });
+  };
+  const handleDown = () => {
+    dispatch({ type: "update", step: -1 });
+  };
+  const handleReset = () => {
+    dispatch({ type: "reset", init: 0 });
   };
 
   return (
     <>
-      <button onClick={handleClick}>カウント</button>
+      <button onClick={handleUp}>カウントアップ</button>
+      <button onClick={handleDown}>カウントダウン</button>
+      <button onClick={handleReset}>カウントリセット</button>
       <p>{state.count}回、クリックされました。</p>
     </>
   );
