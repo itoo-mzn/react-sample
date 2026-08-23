b7fc552b7245d22aa751ab0eef0573e1759e0b0e
Takuya Ito <ito0804takuya@gmail.com>
2026-08-23 17:01:58 +0900

    リセット機能追加

--- src/JotaiCounter.jsx
+++ src/JotaiCounter.jsx
@@ -1,8 +1,10 @@
 import { useAtom } from "jotai";
+import { useResetAtom } from "jotai/utils";
 import { counterAtom } from "./atom";
 
 export default function JotaiCounter() {
   const [counter, setCounter] = useAtom(counterAtom);
+  const resetCounter = useResetAtom(counterAtom);
 
   const handleClick = () => {
     setCounter((c) => c + 1);
@@ -10,6 +12,7 @@ export default function JotaiCounter() {
   return (
     <>
       <button onClick={handleClick}>カウント</button>
+      <button onClick={resetCounter}>リセット</button>
       <p>{counter}回、クリックされました。</p>
     </>
   );
--- src/atom.js
+++ src/atom.js
@@ -1,3 +1,3 @@
-import { atom } from "jotai";
+import { atomWithReset } from "jotai/utils";
 
-export const counterAtom = atom(0);
+export const counterAtom = atomWithReset(0);
