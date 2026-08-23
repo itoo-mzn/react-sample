623637839871df277cdd2aed71532d8eb449da88
Takuya Ito <ito0804takuya@gmail.com>
2026-08-23 18:43:20 +0900

    Jotaiを使ったTODOアプリ

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,9 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import JotaiCounter from "./JotaiCounter.jsx";
+import JotaiTodo from "./JotaiTodo.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <JotaiCounter />
+    <JotaiTodo />
   </StrictMode>,
 );
--- src/store/atom.js
+++ src/store/atom.js
@@ -1,3 +1,22 @@
 import { atomWithReset } from "jotai/utils";
+import { atom } from "jotai";
 
 export const counterAtom = atomWithReset(0);
+
+export const todosAtom = atom([
+  {
+    id: 1,
+    title: "資料作成",
+    isDone: false,
+  },
+  {
+    id: 2,
+    title: "レビュー",
+    isDone: false,
+  },
+]);
+
+export const todoLastIdAtom = atom((get) => {
+  const todos = get(todosAtom);
+  return todos.at(-1)?.id ?? 0;
+});
