00b389be5093f9649ed03e13541a69785ac2a99e
Takuya Ito <ito0804takuya@gmail.com>
2026-08-25 07:07:52 +0900

    メモ化

--- src/HookMemo.jsx
+++ src/HookMemo.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useCallback, useMemo, useState } from "react";
 import { MyButton, MyCounter } from "./HookMemoChild";
 
 const sleep = (delay) => {
@@ -9,15 +9,16 @@ const sleep = (delay) => {
 export default function HookMemo() {
   const [count1, setCount1] = useState(0);
   const [count2, setCount2] = useState(0);
-  const increment = () => setCount1((c) => c + 1);
-  const decrement = () => setCount2((c) => c - 1);
+  const increment = useCallback(() => setCount1((c) => c + 1), []);
+  const decrement = useCallback(() => setCount2((c) => c - 1), []);
 
   // count2を更新（カウントダウン）しても、（コンポーネントが再描画されるため）
   // count2は関係ないこの関数が実行され、応答が遅くなる
-  const heavyProcess = () => {
+  // →useMemoを使ってcount1が変化したときだけ実行されるようにする
+  const heavyProcess = useMemo(() => {
     sleep(1000);
     return count1 + 100;
-  };
+  }, [count1]);
 
   return (
     <>
@@ -26,7 +27,7 @@ export default function HookMemo() {
           カウントアップ
         </MyButton>
         <MyCounter id="c1" value={count1} />
-        <p>{heavyProcess()}</p>
+        <p>{heavyProcess}</p>
       </div>
       <div>
         <MyButton id="btn2" handleClick={decrement}>
