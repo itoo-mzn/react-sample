99814ba1311646c2e84ae7dca035b53494b80e19
Takuya Ito <ito0804takuya@gmail.com>
2026-08-26 07:59:36 +0900

    useTrandition前

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,9 @@
 // import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import HookMemo from "./HookMemo.jsx";
+import HookTransition from "./HookTransition.jsx";
 
 createRoot(document.getElementById("root")).render(
   // <StrictMode>
-  <HookMemo />,
+  <HookTransition />,
   // </StrictMode>,
 );
