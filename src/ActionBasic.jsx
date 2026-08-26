db76c7a69d1595d5a2cb839ce3282f463f9f8264
Takuya Ito <ito0804takuya@gmail.com>
2026-08-27 08:03:21 +0900

    useOptimisticを使った楽観的更新

--- src/ActionBasic.jsx
+++ src/ActionBasic.jsx
@@ -1,12 +1,32 @@
-import { useActionState, useState } from "react";
+import { useActionState, useOptimistic, useState } from "react";
 import { updateForm } from "./action";
 
 export default function BookForm() {
   const [books, setBooks] = useState([]);
 
+  // 楽観的Stateを用意（booksのコピー）
+  const [optimisticBooks, setOptimisticBooks] = useOptimistic(
+    books,
+    (prevBooks, formData) => {
+      return [
+        ...prevBooks,
+        {
+          id: crypto.randomUUID(),
+          title: formData.get("title"),
+          price: formData.get("price"),
+          published: formData.get("published"),
+          updating: true,
+        },
+      ];
+    },
+  );
+
   const [error, submitAction, isPending] = useActionState(
     // サブミット時に呼び出される関数
     async (prevState, formData) => {
+      // 楽観的更新
+      setOptimisticBooks(formData);
+
       const { result, errors } = await updateForm({
         id: crypto.randomUUID(),
         title: formData.get("title"),
@@ -52,9 +72,10 @@ export default function BookForm() {
       </div>
 
       <ul>
-        {books.map((book) => (
+        {optimisticBooks.map((book) => (
           <li key={book.id}>
             {book.title} - {book.price}円 - {book.published}
+            {book.updating && <small>(Updating...)</small>}
           </li>
         ))}
       </ul>
